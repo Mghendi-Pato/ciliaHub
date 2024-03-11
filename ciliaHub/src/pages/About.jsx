e40edabf6471d2a0b6fe87db286d73d6ea2f5a2a
Mghendi-Pato <mwamburipatrick8@gmail.com>
2024-03-11 14:49:46 +0300

    Worked on text on about

--- ciliaHub/src/pages/About.jsx
+++ ciliaHub/src/pages/About.jsx
@@ -1,5 +1,6 @@
 import { Stack, Typography } from "@mui/material";
 import Button from "../components/CustomButton";
+import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAlt";
 
 const About = () => {
   return (
@@ -16,15 +17,80 @@ const About = () => {
         </Typography>
         <Typography
           sx={{
-            textAlign: "center",
             color: "5F5D5F",
-            width: { xs: "95vw", md: "30vw" },
+            width: { xs: "95vw", md: "50vw" },
           }}>
-          A dedicated Africa-based technology company with quality services
-          guaranteeing satisfaction of customers' needs worldwide. With a
-          vibrant team of experienced professionals, ciliaHub delivers results
-          by adhering to best practices and embracing the latest technologies.
+          Welcome to ciliaHub Technologies – a distinguished Africa-based
+          technology company dedicated to delivering top-notch services that
+          meet the diverse needs of clients across the globe. Our vibrant team
+          of seasoned professionals is committed to providing exceptional
+          solutions by adhering to industry best practices and embracing the
+          latest technologies.
         </Typography>
+        <Typography
+          sx={{
+            color: "5F5D5F",
+            width: { xs: "95vw", md: "50vw" },
+          }}>
+          At ciliaHub, we take pride in our innovative approach, creative
+          solutions, and unwavering commitment to customer satisfaction. Our
+          experienced professionals ensure the highest quality results,
+          positioning ciliaHub as a trusted partner for businesses seeking
+          cutting-edge UI/UX design, robust frontend development, and powerful
+          backend solutions.
+        </Typography>
+        <Typography
+          sx={{
+            color: "5F5D5F",
+            width: { xs: "95vw", md: "50vw" },
+            fontWeight: "bold",
+          }}>
+          Why Choose ciliaHub Technologies?
+        </Typography>
+        <Stack
+          component="ul"
+          spacing={1}
+          sx={{
+            color: "#5F5D5F",
+            width: { xs: "95vw", md: "50vw" },
+            listStyleType: "none",
+            paddingLeft: 0,
+          }}>
+          <li>
+            <Stack direction="row" alignItems="center" spacing={1}>
+              <SentimentSatisfiedAltIcon sx={{ color: "#C6171E" }} />
+              <Typography>
+                Comprehensive technology solutions tailored to your
+                requirements.
+              </Typography>
+            </Stack>
+          </li>
+          <li>
+            <Stack direction="row" alignItems="center" spacing={1}>
+              <SentimentSatisfiedAltIcon sx={{ color: "#C6171E" }} />
+              <Typography>
+                A dedicated team of experienced professionals.
+              </Typography>
+            </Stack>
+          </li>
+          <li>
+            <Stack direction="row" alignItems="center" spacing={1}>
+              <SentimentSatisfiedAltIcon sx={{ color: "#C6171E" }} />
+              <Typography>
+                Adherence to industry best practices and the latest
+                technologies.
+              </Typography>
+            </Stack>
+          </li>
+          <li>
+            <Stack direction="row" alignItems="center" spacing={1}>
+              <SentimentSatisfiedAltIcon sx={{ color: "#C6171E" }} />
+              <Typography>
+                Commitment to innovation, creativity, and excellence.
+              </Typography>
+            </Stack>
+          </li>
+        </Stack>
         <Button text="Get In Touch" />
       </Stack>
     </>
